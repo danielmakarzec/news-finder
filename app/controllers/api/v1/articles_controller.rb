926ad1478529db3544e28779438cbf4ca3e054f1
daniel makarzec <danielmakarzec@gmail.com>
2020-01-30 17:05:47 +0000

    create strong params and CRUD methods in api controller file

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,2 +1,36 @@
-class Api::V1::ArticlesController < ApplicationController
+class Api::V1::ArticlesController < ActionController::Base
+  def index
+    # @articles = Article.order(created_at: :desc)
+    render json: Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    render json: @article
+  end
+
+  def create
+    article = Article.create(article_params)
+    render json: article
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(
+      :author,
+      :content,
+      :description,
+      :publishedAt,
+      :sourceName,
+      :title,
+      :url,
+      :urlToImage
+    )
+  end
 end
