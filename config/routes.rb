f6ffd2db20f6a340a86f38668a9a247385e39825
daniel makarzec <danielmakarzec@gmail.com>
2020-02-01 14:34:57 +0000

    add missing route for /favourite

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'articles#home'
+  get '/favourite', to: 'articles#home'
   namespace :api do
     namespace :v1 do
       resources :articles, only: [:index, :show, :create, :destroy]
